065b05dfa9fc908c869c70812974d82d31c814b9
ostapenko-kostia <ostapenkokpersonal@gmail.com>
2025-07-25 19:45:36 +0300

    remove projects

--- src/components/shared/home/portfolio/portfolio.data.ts
+++ src/components/shared/home/portfolio/portfolio.data.ts
@@ -43,26 +43,5 @@ export const PORTFOLIO: IPortfolioItem[] = [
 			'Fabriq is an online store specializing in high-quality statues and sculptures, offering a wide range of art pieces for collectors, decorators, and art enthusiasts.',
 		image: '/projects/fabriq.avif',
 		link: 'https://fabriq.com.pl'
-	},
-	{
-		title: 'Bluetongue',
-		description:
-			'Bluetongue is a SaaS platform designed to provide a seamless and interactive experience for reading digital books.',
-		image: '/projects/bluetongue.avif',
-		link: 'https://bluetongue.com.ua'
-	},
-	{
-		title: 'Ulethai',
-		description:
-			'Ulethai is a Thai company that provides car rental services. The site includes filters, car search, booking and much more',
-		image: '/projects/ulethai.avif',
-		link: 'https://ulethai.com'
-	},
-	{
-		title: 'My IQ Rank',
-		description:
-			'My IQ Rank is an online platform dedicated to providing IQ testing services. The website offers users the opportunity to take scientifically designed IQ tests to measure their cognitive abilities.',
-		image: '/projects/my-iq-rank.avif',
-		link: 'https://myiqrank.com'
 	}
 ]
